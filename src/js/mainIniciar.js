741711cbfbc4e691642edc155b7753c0514a6b3d
milelym <milelymarcial@gmail.com>
2018-07-17 16:38:13 -0400

    modificaciones en inicio de sesion

--- src/js/mainIniciar.js
+++ src/js/mainIniciar.js
@@ -55,26 +55,26 @@ function cerrarSesion(){
   })
 }
 //iniciar sesion con google
-function googleLoginWithFirebase(){
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(result);
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-  }
+  function googleLoginWithFirebase(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    }
+    
   //facebook
 function facebookLoginWithFirebase(){
   var provider = new firebase.auth.FacebookAuthProvider();
